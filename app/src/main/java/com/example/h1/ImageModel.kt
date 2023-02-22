acfd9a8b3fcb9c75763da716daca2050c47a4b6d
Vaashkar Paul <vaashkarpaul2050@gmail.com>
2023-02-22 16:47:12 +0530

    JUST SAVING THE STRUCT

---
+++ app/src/main/java/com/example/h1/ImageModel.kt
@@ -0,0 +1,10 @@
+package com.example.h1
+
+class ImageModel(
+    val vehicleName: String,
+    val vehicleUrl: String,
+    val vehicleOwnner: String,
+    val vehiclenum: Int,
+    val vehiclemileage: Float,
+    val userId: String // ID of the user who uploaded the image
+)
---
+++ app/src/main/java/com/example/h1/Initial.kt
@@ -0,0 +1,11 @@
+package com.example.h1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Initial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
