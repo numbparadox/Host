ae5bd9fc411722a4081f153370d2111af50bd39b
Vaashkar Paul <vaashkarpaul2050@gmail.com>
2023-02-27 01:09:19 +0530

    0.0.0.0.0.1

--- app/src/main/java/com/example/h1/Initial.kt
+++ app/src/main/java/com/example/h1/Initial.kt
@@ -1,11 +1,18 @@
 package com.example.h1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.h1.databinding.ActivityInitialBinding
 
 class Initial : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInitialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial)
+        setContentView(binding.root)
+
+        val intent = Intent(this,UploadImage::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/h1/UploadImage.kt
@@ -0,0 +1,11 @@
+package com.example.h1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadImage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_image)
+    }
+}
